0f0582d4070520f0a4339f1da975e897369127de
Gautam Sharma <gautamsharma7234@gmail.com>
2024-10-30 22:37:07 -0500

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,12 @@
+#include <sstream>
 #include <iostream>
+#include <optional>
+
+
+std::optional<int> cmdExit(std::istringstream& iss);
+
+std::string cmdEcho(std::istringstream& iss);
+
 
 int main() {
     // Flush after every std::cout / std:cerr
@@ -11,11 +19,40 @@ int main() {
         std::string input;
         std::getline(std::cin, input);
 
-        if (input == "exit 0")
-            return 0;
+        std::istringstream iss(input);
+        iss >> input;
 
-        std::cerr << input << ": command not found\n";
+        if (input == "exit") {
+            if (auto ret = cmdExit(iss))
+                return *ret;
+        } else if (input == "echo") {
+            std::cout << cmdEcho(iss) << std::endl;
+        } else {
+            std::cerr << input << ": command not found\n";
+        }
     }
 
     return 0;
+}
+
+std::optional<int> cmdExit(std::istringstream& iss) {
+    std::string input;
+
+    try {
+        iss >> input;
+        return stoi(input);
+    } catch (const std::invalid_argument& e) {
+        std::cerr << "Invalid argument: " << e.what() << '\n';
+    } catch (const std::out_of_range& e) {
+        std::cerr << "Out of range: " << e.what() << '\n';
+    }
+
+    return std::nullopt;
+}
+
+std::string cmdEcho(std::istringstream& iss) {
+    std::string output;
+    std::getline(iss, output);
+    output.erase(0, output.find_first_not_of(" \t\n"));
+    return output;
 }
