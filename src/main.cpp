8ede45f85c656ee679ef4fee3cef7594cef2ece6
Gautam Sharma <gautamsharma7234@gmail.com>
2024-10-30 20:18:15 -0500

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,12 @@ int main() {
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
 
-    std::cout << "$ ";
+    while (true) {
+        std::cout << "$ ";
 
-    std::string input;
-    std::getline(std::cin, input);
+        std::string input;
+        std::getline(std::cin, input);
 
-    std::cerr << input << ": command not found\n";
+        std::cerr << input << ": command not found\n";
+    }
 }
