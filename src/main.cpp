dcc5f750e48f4cc56b98f4c88814bb383b237b57
Gautam Sharma <gautamsharma7234@gmail.com>
2024-10-31 18:25:21 -0500

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,21 @@
 #include <sstream>
 #include <iostream>
 #include <optional>
+#include <functional>
+#include <unordered_map>
 
-
-std::optional<int> cmdExit(std::istringstream& iss);
-
-std::string cmdEcho(std::istringstream& iss);
-
+void cmdEcho(std::istringstream& iss);
 
 int main() {
     // Flush after every std::cout / std:cerr
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
+
+    std::unordered_map<std::string, std::function<void(std::istringstream&)>> cmds = {
+        {"type", nullptr},
+        {"exit", nullptr},
+        {"echo", cmdEcho},
+    };
 
     while (true) {
         std::cout << "$ ";
@@ -23,36 +27,38 @@ int main() {
         iss >> input;
 
         if (input == "exit") {
-            if (auto ret = cmdExit(iss))
-                return *ret;
-        } else if (input == "echo") {
-            std::cout << cmdEcho(iss) << std::endl;
+            iss >> input;
+            try {
+                return stoi(input);
+            } catch (const std::invalid_argument& e) {
+                std::cerr << "Invalid argument: " << e.what() << '\n';
+            } catch (const std::out_of_range& e) {
+                std::cerr << "Out of range: " << e.what() << '\n';
+            }
+        } else if (input == "type") {
+            while (iss >> input) {
+                if (cmds.find(input) != cmds.end())
+                    std::cout << input << " is a shell builtin\n";
+                else
+                    std::cerr << input << ": not found\n";
+            }
         } else {
-            std::cerr << input << ": command not found\n";
+            auto it = cmds.find(input);
+
+            if (it != cmds.end())
+                it->second(iss);
+            else
+                std::cerr << input << ": command not found\n";
         }
     }
 
     return 0;
 }
 
-std::optional<int> cmdExit(std::istringstream& iss) {
-    std::string input;
-
-    try {
-        iss >> input;
-        return stoi(input);
-    } catch (const std::invalid_argument& e) {
-        std::cerr << "Invalid argument: " << e.what() << '\n';
-    } catch (const std::out_of_range& e) {
-        std::cerr << "Out of range: " << e.what() << '\n';
-    }
-
-    return std::nullopt;
-}
-
-std::string cmdEcho(std::istringstream& iss) {
+void cmdEcho(std::istringstream& iss) {
     std::string output;
+
     std::getline(iss, output);
     output.erase(0, output.find_first_not_of(" \t\n"));
-    return output;
+    std::cout << output << '\n';
 }
